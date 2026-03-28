7d05f1088be2969f20360207b327adef0a9467fe
markr <markr@users.noreply.github.com>
2026-03-28 15:27:51 +0700

    fix(prisma): use dummy db url during build

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,14 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+
+// We provide a dummy URL for the build process, 
+// as the real DATABASE_URL is not available at this stage.
+const databaseUrl =
+  process.env.DATABASE_URL || "postgresql://dummy:dummy@dummy:5432/dummy";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: databaseUrl,
   },
 });
